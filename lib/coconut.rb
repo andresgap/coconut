f8809e5f28ebde548b6eec5ca1efa8b11c227ecf
Andres Hernandez <ahernandez@growthaccelerationpartners.com>
2016-04-01 15:52:22 -0600

    Add cache service

--- lib/coconut.rb
+++ lib/coconut.rb
@@ -7,6 +7,7 @@ module Coconut
 end
 
 require_relative "coconut/version"
+require_relative 'coconut/cache_service'
 require_relative 'coconut/swap_service'
 require_relative 'coconut/fetch_service'
 require_relative 'coconut/command_interface'
